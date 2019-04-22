d6274993e3bdac072b0aa7f874d9d6c231b07768
trojanh <rajan.tiwari@kiprosh.com>
2019-04-22 17:43:03 +0530

    Register operator route for shopify

--- connectors-shopify/client/index.js
+++ connectors-shopify/client/index.js
@@ -1,1 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingBag } from "@fortawesome/free-solid-svg-icons";
 import "./settings/shopify";
+
+registerOperatorRoute({
+  isNavigationLink: true,
+  isSetting: true,
+  mainComponent: "shopifyConnectSettings",
+  path: "/shopify-connect-settings",
+  // eslint-disable-next-line react/display-name
+  SidebarIconComponent: (props) => <FontAwesomeIcon icon={faShoppingBag} {...props} />,
+  sidebarI18nLabel: "reaction-connectors-shopify.admin.dashboard.shopifyConnectLabel"
+});
