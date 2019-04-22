d622aada66b346381e11aaeeb219c69c78aab9a7
trojanh <rajan.tiwari@kiprosh.com>
2019-04-22 17:21:42 +0530

    Register operator route for connector

--- connectors/client/index.js
+++ connectors/client/index.js
@@ -1,1 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlug } from "@fortawesome/free-solid-svg-icons";
 import "./templates";
+
+registerOperatorRoute({
+  isNavigationLink: true,
+  isSetting: true,
+  mainComponent: "connectorSettings",
+  path: "/connector-settings",
+  // eslint-disable-next-line react/display-name
+  SidebarIconComponent: (props) => <FontAwesomeIcon icon={faPlug} {...props} />,
+  sidebarI18nLabel: "reaction-shipping.admin.dashboard.shippingLabel"
+});
