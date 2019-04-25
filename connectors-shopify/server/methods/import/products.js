b16d41900ccfc426592f1eccf6473c590806aa52
trojanh <rajan.tiwari@kiprosh.com>
2019-04-25 17:25:48 +0530

    Map Shopify quantity to reaction

--- connectors-shopify/server/methods/import/products.js
+++ connectors-shopify/server/methods/import/products.js
@@ -85,6 +85,8 @@ function createReactionProductFromShopifyProduct(options) {
  */
 function createReactionVariantFromShopifyVariant(options) {
   const { shopifyVariant, variant, index, ancestors, shopId } = options;
+  const inventoryInStock = shopifyVariant.inventory_quantity >= 0 ? shopifyVariant.inventory_quantity : 0;
+
   const reactionVariant = {
     ancestors,
     barcode: shopifyVariant.barcode,
@@ -94,7 +96,8 @@ function createReactionVariantFromShopifyVariant(options) {
     index,
     inventoryManagement: true,
     inventoryPolicy: shopifyVariant.inventory_policy === "deny",
-    inventoryQuantity: shopifyVariant.inventory_quantity >= 0 ? shopifyVariant.inventory_quantity : 0,
+    inventoryInStock,
+    inventoryAvailableToSell: inventoryInStock,
     isDeleted: false,
     isVisible: true,
     length: 0,
@@ -338,7 +341,7 @@ export const methods = {
                   ownerId: Meteor.userId(),
                   productId: reactionProductId,
                   shopId,
-                  priority: productImage.position, // Shopify index positions starting at 1.
+                  priority: productImage.position-1, // Shopify index positions starting at 1.
                   toGrid: 1
                 });
               }
